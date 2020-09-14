0f3305debee46a03c131e78fa77a33223a5568e6
EC2 Default User <ec2-user@ip-172-31-32-188.ec2.internal>
2020-09-14 08:28:36 +0000

    day5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @book = @user.posts.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render "edit"
+    end
+  end
+  
+  private 
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users 
+  # ここにルートパス設定
+  root 'home#home'
+  get  "home/about"  => "home#index"
+  resources :books, only: [:index, :create, :show, :destroy, :update, :edit]
+  resources :users, only: [:index, :create, :show, :update, :edit]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
